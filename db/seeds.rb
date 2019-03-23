cde69baff4020ff2de73e7eacb09d6c01b92aa06
Chris Leung <pserleung@gmail.com>
2019-03-23 17:09:21 +0900

    add expense category to seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Expensecategory.create(name: 'Food')
+Expensecategory.create(name: 'Entertainment')
+Expensecategory.create(name: 'Gift')
+Expensecategory.create(name: 'Medicine')
+Expensecategory.create(name: 'Travel')
+Expensecategory.create(name: 'Misc')
